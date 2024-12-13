048c2771269344714467a825714ad2d34f251bd4
Carl-Dimitri Fogue <147193447+akacarlll@users.noreply.github.com>
2024-12-13 14:01:16 +0100

    New pipeline registry

--- src/lyrics_generator/pipeline_registry.py
+++ src/lyrics_generator/pipeline_registry.py
@@ -7,6 +7,9 @@ from typing import Dict
 from kedro.pipeline import Pipeline
 from  lyrics_generator.pipelines.preprocessing.pipeline import create_pipeline
 from  lyrics_generator.pipelines.add_features.pipeline import create_pipeline2
+from  lyrics_generator.pipelines.data_analysis.pipeline import create_pipeline3
+from  lyrics_generator.pipelines.concatenate.pipeline import create_pipeline4
+
   # Importer create_pipeline
 # from .pipelines import data_cleaning as dc_pipeline
 
@@ -31,18 +34,23 @@ def register_pipelines() -> Dict[str, Pipeline]:
     # Chemins des dossiers contenant les fichiers CSV
     folder_path1 = "C:/Users/carlf/Documents/GitHub/lyrics_generator/data/01_raw"
     folder_path2 = "C:/Users/carlf/Documents/GitHub/lyrics_generator/data/02_intermediate"
+    folder_path3 = "C:/Users/carlf/Documents/GitHub/lyrics_generator/data/03_advanced"
 
     # Créer les pipelines individuels
     pipeline1 = create_pipeline(folder_path1)
     pipeline2 = create_pipeline2(folder_path2)
+    pipeline3 = create_pipeline3(folder_path3)
+    pipeline4 = create_pipeline4(folder_path3)
 
     # Fusionner les pipelines
-    combined_pipeline = pipeline1 + pipeline2
+    combined_pipeline = pipeline1 + pipeline2 + pipeline3 + pipeline4
 
     # Définir les pipelines
     pipelines = {
         "my_pipeline": pipeline1,
         "second_pipeline": pipeline2,
+        "third_pipeline": pipeline3,
+        "fourth_pipeline": pipeline4,
         "combined_pipeline": combined_pipeline,
         "__default__": combined_pipeline
     }
